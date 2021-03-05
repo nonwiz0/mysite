1ec1cb275f7cfe75a04de2b5cbf662875e29cf73
chanrose <bdchanbroset@gmail.com>
2021-03-05 09:49:45 +0700

    update url fx

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Question
 
@@ -12,10 +12,11 @@ def index(request):
     #return HttpResponse(output)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
     # return HttpResponse("You're looking at question %s." % question_id)
 
