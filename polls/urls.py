b23834f3b6496a807e2f57ba0b2d6547d8f88350
chanrose <bdchanbroset@gmail.com>
2021-03-09 15:50:20 +0700

    update view

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ Created on Mar 4, 2021
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name="index"),
     path('<int:question_id>/', views.detail, name='detail'),
