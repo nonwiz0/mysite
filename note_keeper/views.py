1b3416b83f89b6a88aa1d04a07fa08e1e6b276f2
Chanbroset <bdchanbroset@gmail.com>
2021-03-18 22:58:40 +0700

    add toast

--- note_keeper/views.py
+++ note_keeper/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.views import generic
 from .models import Note
 from django.utils import timezone
+from django.contrib import messages
+
 # Create your views here.
 
 
@@ -24,6 +26,7 @@ class EditView(generic.DetailView):
     template_name = 'note_keeper/edit_note.html'
     
 def delete_note(request, note_id):
+    messages.info(request, 'Note ID #{} has been deleted'.format(note_id))
     Note.objects.filter(pk=note_id).delete()
     return HttpResponseRedirect(reverse('note_keeper:index'))
 
@@ -33,6 +36,7 @@ def create_note(request):
     now = timezone.now()
     note = Note(title=title, content=content, doc=now, modify_date=now)
     note.save()
+    messages.info(request, 'Note: {} has been saved'.format(title))
     return HttpResponseRedirect(reverse('note_keeper:index'))
 
 def update_note(request):
@@ -40,8 +44,13 @@ def update_note(request):
     title = request.POST['title']
     content = request.POST['content']
     note = Note.objects.get(pk=note_id)
-    note.modify_date = timezone.now()
-    note.title = title
-    note.content = content 
-    note.save()
+
+    if note.title == title and note.content == content:
+       messages.info(request, "No change detected")
+    else:
+        note.modify_date = timezone.now()
+        note.title = title
+        note.content = content 
+        note.save()
+        messages.info(request, 'Note #{} has been updated'.format(note_id))
     return HttpResponseRedirect(reverse('note_keeper:note', args=[note_id]))
