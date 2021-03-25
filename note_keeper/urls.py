7a611f5502071ea18a5cef4e24525d9f9a332ed2
Chanbroset <bdchanbroset@gmail.com>
2021-03-26 00:45:05 +0700

    update style and view

--- note_keeper/urls.py
+++ note_keeper/urls.py
@@ -6,9 +6,9 @@ app_name = 'note_keeper'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('create_note/', views.create_note, name='create_note'),
-    path('<int:note_id>/delete_note/', views.delete_note, name='delete_note'),
-    path('<int:pk>/', views.DetailView.as_view(), name='note'),
-    path('<int:pk>/edit', views.EditView.as_view(), name='edit_note'),
+    path('<str:note_title>/delete_note/', views.delete_note, name='delete_note'),
+    path('<str:pk>/', views.DetailView.as_view(), name='note'),
+    path('<str:pk>/edit', views.EditView.as_view(), name='edit_note'),
     path('update_note', views.update_note, name='update_note')
 ]
 
